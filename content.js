b658b3fdfd64a89bd2a9fbdd194dcdb6f8611e60
Thakuei <c1247183@st.kanazawa-it.ac.jp>
2024-04-12 11:10:58 +0900

    エンターキーを押したらページ入力ができる

---
+++ content.js
@@ -0,0 +1,5 @@
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    window.location.href = "https://navi.mars.kanazawa-it.ac.jp/portal/student/KITP0010001";
+  }
+});
