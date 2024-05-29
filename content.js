244db8c4f6a09e7bfb7f594cf16789f9deba2581
Thakuei <c1247183@st.kanazawa-it.ac.jp>
2024-05-29 17:40:32 +0900

    ざわたくのコードを持ってきた

--- content.js
+++ content.js
@@ -1,5 +1,9 @@
-document.addEventListener('keydown', function(event) {
-  if (event.key === 'Enter') {
-    window.location.href = "https://navi.mars.kanazawa-it.ac.jp/portal/student/KITP0010001";
+document.body.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+      return func1();
   }
 });
+
+var func1 = function () {
+  document.getElementById("StudentLoginBtn").click();
+}
